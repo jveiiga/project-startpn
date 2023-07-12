fbcbcc820d32f7f33e7de62a9b5ef70a5738b8e3
jveiiga <jeferson.veiiga@icloud.com>
2023-07-12 18:34:40 -0300

    Implementation of token verification and validation middleware. Controller login for user lookup and username and password checks.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express")
 const ControllerUsers = require("./app/controllers/ControllerUsers")
-
+const authenticationMiddleware = require("./app/middlewares/auth")
 const routes = Router()
 
 routes.post("/user", ControllerUsers.storage)
+routes.post("/login", authenticationMiddleware, ControllerUsers.login)
 
 module.exports = routes
